a2c70e454543718c00568688290c51c0419f0b7f
Mateusz Malanowski <38919740+m-malanowski@users.noreply.github.com>
2020-07-29 10:33:30 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import 'bootstrap'
 // import './scss/app.scss';
-// // import './js/scroll-effect'
-// // import './js/barba'
-// import './js/balls'
-// // import './js/svg-text'
-// // import './js/projects'
-// import './js/cursor'
-//
-//
-//
 // import Highway from '@dogstudio/highway';
 // import Fade from './js/page-tranitions';
 // import CustomRenderer from './js/custom-renderer.js';
